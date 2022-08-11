75f1af3e818755e328fcb1ae89e8e387c622181e
luxxyanjaymabar <110882963+luxxyanjaymabar@users.noreply.github.com>
2022-08-11 20:05:29 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 •Fitur Ini Erorr Ngentot, Download Sendiri Di Snaptik.com
 
-handler.customPrefix = /^(.tiktok)$/i
+handler.customPrefix = /^(.tt|.tiktok|tt|tiktok)$/i
 handler.command = new RegExp
 
 handler.limit = false
